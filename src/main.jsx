ba363cb3c7edbcaf1768f0a0387e09f6d310e5c8
aitbwbooking-ctrl <ai.tbw.booking@gmail.com>
2025-12-15 19:37:13 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// TBW Runtime (side-effect, pokreće se sam)
 import "./tbw/runtime/TBWRuntime";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
